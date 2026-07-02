03ef80ff38833d866feec570b2a8f56efd531dd0
Tergel Norjinkhorloo <146158863+huurhuntergele@users.noreply.github.com>
2026-07-02 20:37:30 +0900

    Add files via upload

---
+++ 02-obstacle-avoidance-module-with-alarm/02-obstacle-avoidance-module-with-alarm.ino
@@ -0,0 +1,24 @@
+const int sensorPin = 32;
+const int buzzerPin = 12;
+int avoidState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+  Serial.print("\nesp32 is ready!");
+  pinMode(sensorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+}
+
+void loop() {
+  avoidState = digitalRead(sensorPin);
+  if(avoidState == 0){
+    digitalWrite(buzzerPin, HIGH);
+  }else{
+    digitalWrite(buzzerPin, LOW);
+  }
+    
+  
+
+}
